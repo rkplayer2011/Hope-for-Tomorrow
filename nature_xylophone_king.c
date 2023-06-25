408783b359dfdc890bb2be316eec63b1741280c8
rkplayer2011 <136753235+rkplayer2011@users.noreply.github.com>
2023-06-25 14:38:15 +0700

    Refactor SQL queries

---
+++ nature_xylophone_king.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+    //This program will output the message "Hope for Tomorrow"
+
+    //Declare variables
+    char ch1 = 'H';
+    char ch2 = 'o';
+    char ch3 = 'p';
+    char ch4 = 'e';
+    char ch5 = ' ';
+    char ch6 = 'f';
+    char ch7 = 'o';
+    char ch8 = 'r';
+    char ch9 = ' ';
+    char ch10 = 'T';
+    char ch11 = 'o';
+    char ch12 = 'm';
+    char ch13 = 'o';
+    char ch14 = 'r';
+    char ch15 = 'r';
+    char ch16 = 'o';
+    char ch17 = 'w';
+
+    //Print the message
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",ch1,ch2,ch3,ch4,ch5,ch6,ch7,ch8,ch9,ch10,ch11,ch12,ch13,ch14,ch15,ch16,ch17);
+
+    return 0;
+}
