6a5376cb6d9c7eec36969b3062e2cf5e699eb110
rkplayer2011 <136753235+rkplayer2011@users.noreply.github.com>
2023-06-27 11:35:28 +0700

    Fix data consistency issues

---
+++ sun_lion_piano.rb
@@ -0,0 +1,47 @@
+# 'Hope for Tomorrow'
+
+# A hopeful song to inspire a brighter future
+
+# Verse 1
+
+lyrics = "In the morning I am waking, feeling the sun rise
+Ready to start a brand new day, no more bad surprise
+Though I'm feeling so uneasy, I open my eyes wide
+And I'm strong and faithful, hoping for tomorrow to arrive"
+
+# Chorus
+
+chorus = "Cause I hope for tomorrow, when the night is gone
+I'll be closer to my dreams, so I can carry on
+The future's waiting for me, I will take it on
+Cause I'm longing for the light, hope for tomorrow"
+
+# Verse 2
+
+lyrics_2 = "There are days full of sunshine yet others veiled in rain
+And some that quietly pass by on an endless train
+But I'm not ready to give up, I'm searching for the flame
+To keep me going, light my way, hope for tomorrow's name"
+
+# Bridge
+
+bridge = "There's no time for reflecting, just focus on what's right
+I'll keep moving forward, no given night or light
+I want to live for the moment and feel the joy inside
+Open up to the world and find what will be mine"
+
+# Final Chorus
+
+final_chorus = "Cause I hope for tomorrow, when the night is gone
+I'll be closer to my dreams, so I can carry on
+The future's waiting for me, I will take it on
+Cause I'm longing for the light, hope for tomorrow"
+
+
+# Outputting the lyrics
+
+puts lyrics
+puts chorus
+puts lyrics_2
+puts bridge
+puts final_chorus
